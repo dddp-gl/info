c847f83da185340deb5526b78a39cfeb20cd3e16
glinfo <glinfo@dddp-gl.org>
2025-06-29 11:06:53 +0900

    1st commit

---
+++ reference/discription.js
@@ -0,0 +1,62 @@
+export const discription = {
+  area_list_title: `<p class="bg_yellow"><strong>Common Meeting Viewing Site</strong>/by Digital Direct Democracy Platform</p>
+<p><strong>Area_Code2023 List</strong></p>`,
+  area_list_discription: ``,
+  meeting_list_tutle: `<p><strong>Meeting List in the area</strong></p>`,
+  meeting_list_discription: `<p>(Note 1)The "status" column indicates whether the meeting is open or closed.
+<br>(Note 2)The meeting number "xxyyyyzzzwnn"(example:gl2023ecop01) indicates xx:area_code, yyyy:meeting year, zzzw:classification code, and nn:number.
+<br>The manager of this page will decide the meeting number.
+<br>(Note 3) "zzzw" in the conference number indicates the classification code of the social issue.
+<br>Classification of social issues is based on a combination of field classification and PDCA cycle.
+<br>(1) Field classification (zzz)
+<br>1_dom: domestic consensus building, freedom of speech, news coverage, elections, information disclosure
+<br>2_eco: economic activity, economic growth, capital, accounting, resources, labor force, working environment, wages, employment,
+technological development, market, contracts, business protection, industry-specific policies
+<br>3_edu: education,research,culture,religion,organization
+<br>4_env: environment
+<br>5_fam: family, home, residence, household finances
+<br>6_hlt:health and welfare
+<br>7_hrg:human rights
+<br>8_int:international solidarity, diplomacy, defense
+<br>9_loc:local autonomy
+<br>10_npx:Non-profit public interest activities
+<br>11_pol: police activity
+<br>12_saf:safety and convenience
+<br>13_sup:common support,tax system, military conscription,community finances
+<br>(2) Classification by Plan-Do-Check-Action cycle (w)
+<br>1_p: creation and review of social issue solution plans
+<br>2_d: pros and cons of operation method
+<br>3_c: investigation of social issues
+<br>4_a: analysis of social issues</p>`,
+  meeting_sheet_title: `<p class="bg_yellow"><strong>Meeting Sheet</strong></p>
+<p>(Note 1)
+</p>`,
+  meeting_sheet_discription: `<p>column1 t:thema. 
+<br>column2 p:the reason of a proposal.
+<br>column3 gb:good/bad point.
+<br>column4 q:question.
+<br>column5 a:answer.
+<br>column6 s:spare.
+<br>column7 c:content.
+ </p>`,
+  reference_list_title: `<p class="bg_yellow"><strong>Reference Sheet</strong></p>
+<p>(Note 1)
+</p>`,
+  reference_list_discription: ``,
+  picture_list_title: `<p class="bg_yellow"><strong>Picture List</strong></p>
+<p>(Note 1)
+</p>`,
+  picture_list_discription: ``,
+  news_sheet_title: `<p class="bg_yellow"><strong>News Sheet</strong></p>
+<p>(Note 1)
+</p>`,
+  news_sheet_discription: ``,
+  survey_list_title: `<p class="bg_yellow"><strong>Survey List</strong></p>
+<p>(Note 1)
+</p>`,
+  survey_list_discription: ``,
+  stock_data_list_title: `<p class="bg_yellow"><strong>Stock Data List</strong></p>
+<p>(Note 1)
+</p>`,
+  stock_data_list_discription: ``,
+}; 
