16e0b997e401fbbfa740a9645566625ef1776f26
glinfo <glinfo@dddp-gl.org>
2025-08-13 18:32:22 +0900

    add survey sheet

---
+++ doc/suevey.js
@@ -0,0 +1,46 @@
+let startDay ="2025/010/01";
+let survey1= {title: "quesion1",
+    yes1: 'reaon1',
+    yes2: 'reaon2',
+    yes3: 'reason3',
+    yes4: 'reason4',
+    no1: 'reason1',
+    no2: 'reason2', 
+    no3: 'reason3',
+    no4: 'reason4',
+    other: 'reason1',
+    other2: 'reason2',
+    other3: 'reason3',
+    other4: 'reason4'
+};
+let survey2= {title: "quesion2",
+    yes1: 'reaon1',
+    yes2: 'reaon2',
+    yes3: 'reason3',
+    yes4: 'reason4',
+    no1: 'reason1',
+    no2: 'reason2', 
+    no3: 'reason3',
+    no4: 'reason4',
+    other: 'reason1',
+    other2: 'reason2',
+    other3: 'reason3',
+    other4: 'reason4'
+};
+let survey3= {title: "quesion3",
+    yes1: 'reaon1',
+    yes2: 'reaon2',
+    yes3: 'reason3',
+    yes4: 'reason4',
+    no1: 'reason1',
+    no2: 'reason2', 
+    no3: 'reason3',
+    no4: 'reason4',
+    other: 'reason1',
+    other2: 'reason2',
+    other3: 'reason3',
+    other4: 'reason4'
+};
+
+
+    
